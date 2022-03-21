5ade8c5b855a4c882f76985f3403f816eee1d459
dsgnfox <dsgnfox@gmail.com>
2022-03-21 20:45:29 +0300

    return babel

--- webpack.config.js
+++ webpack.config.js
@@ -13,14 +13,20 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader', 'postcss-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
     ],
   },
